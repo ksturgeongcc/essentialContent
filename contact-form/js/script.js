1081a9b3774cbd37798119b3e65d541f5c7556c9
Petro Bondarenko <20348148@myclyde.ac.uk>
2024-02-08 17:10:40 +0200

    feat: form validation finish

--- contact-form/js/script.js
+++ contact-form/js/script.js
@@ -6,3 +6,52 @@ const hide__show = () => {
         mapElement.style.display = "none";
     }
   }
+
+
+// form validation
+
+const validateForm = () => {
+
+    const nameErr = document.getElementById('nameError');
+    const emailErr = document.getElementById('emailError');
+    const messageErr = document.getElementById('messageError');
+
+    nameErr.textContent = '';
+    emailErr.textContent = '';
+    messageErr.textContent = '';
+
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const msg = document.getElementById('message').value.trim();
+
+    // name val
+
+    if (name === '') {
+        nameErr.style.color = 'red';
+        nameErr.style.fontSize = '16px';
+        nameErr.textContent = 'Value cannot be empty';
+        event.preventDefault();
+    }
+
+    // email val
+
+    if (email === '') {
+        emailErr.style.color = 'red';
+        emailErr.style.fontSize = '16px';
+        emailErr.textContent = 'Value cannot be empty';
+        event.preventDefault();
+    }
+
+    // msg val
+
+    if (msg === '') {
+        messageErr.style.color = 'red';
+        messageErr.style.fontSize = '16px';
+        messageErr.textContent = 'Value cannot be empty';
+        event.preventDefault();
+    }
+
+}
+
+// nav code
+
